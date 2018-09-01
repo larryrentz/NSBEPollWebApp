ea77cda75808dd8ea4a153d6b1935767d7d83851
quintenrivers <32085810+quintenrivers@users.noreply.github.com>
2018-09-01 12:56:15 -0400

    added basic code for sending messages

--- send-sms.js
+++ send-sms.js
@@ -1,3 +1,10 @@
 const accountSid = process.env.TWILIO_SID;
 const authToken  = process.env.TWILIO_AUTH;
 const client     = require('twilio')(accountSid,authToken);
+
+client.messages.create({
+  to  : null,           // PUT PHONE NUMBER HERE (ENV VARIABLE)
+  from: '+18508088826', // TWILIO FREE NUMBER
+  body: null,           // GET MESSAGE FROM API.AI
+})
+.then((message) => console.log(message.sid));
